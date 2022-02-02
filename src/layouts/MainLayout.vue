0dd8a7ac02e8c8f31d51f5df39130f666eef1b6f
alvaro_car6@hotmail.com <alvaro_car6@hotmail.com>
2022-02-01 19:50:10 -0500

    sda

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1 >{{ fecha }}</h1>
+  <button @click="pressButton()">presione aquí</button>
+</div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    const isVisible = ref(false)
+    const fecha = ref("")
+    const pressButton = () => {
+      isVisible.value = !isVisible.value
+      const date = new Date()
+      fecha.value = date.toString()  
+    }
+    
+
+    return { pressButton, isVisible, fecha }
+  },
+});
+</script>
